c6cd9cfab28a7b0a09f8998bba90abd03568fc9c
MartijnvDijk <m.van.dijk@nijmegen.nl>
2026-02-18 14:50:55 +0100

    chore: revert avi prefill changes

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,11 +4,9 @@ const project = new GemeenteNijmegenCdkApp({
   defaultReleaseBranch: 'main',
   deps: [
     '@gemeentenijmegen/utils',
-    '@gemeentenijmegen/apiclient',
     '@aws-sdk/client-sqs',
     '@aws-lambda-powertools/logger',
     '@aws-lambda-powertools/tracer',
-    'axios',
     'zod',
   ],
   devDeps: [
